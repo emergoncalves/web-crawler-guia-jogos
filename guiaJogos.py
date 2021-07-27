978cf51fb371f7b6ce3edf620080c958306ca436
Emerson Gonçalves <emerson.u.g@gmail.com>
2021-07-27 18:04:01 +0000

    ajuste de erro de caractere

--- guiaJogos.py
+++ guiaJogos.py
@@ -12,17 +12,15 @@ def get_guia_jogos():
     jogos = []
     canais = []
 
-    # Pega informação do jogo
     for jogoSoap in soup.findAll(class_="inner-post-entry"):
             s = jogoSoap
             for jogo in s.findAll(['h3']):
                 jogos.append( jogo.getText() )
 
-    # Pega informação sobre o canal
     for canalSoap in soup.findAll(class_="inner-post-entry"):
             c = canalSoap
             for canal in c.findAll(['span']):
-                if canal.getText().find('►') != 0:
+                if canal.getText().find('>') != 0:
                  canais.append( canal.getText() )
 
     return [list(x) for x in zip(jogos, canais)]
